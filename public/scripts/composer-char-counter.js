9c2e4aa94716a036f7bbcde92222a089c5dc68ce
Clayton <persinger.clayton@gmail.com>
2023-05-22 19:33:34 -0700

    Create js file to update character counter.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  $("#tweet-text").on("input", function(e) {
+    let textLength = e.target.value.length;
+    const counter = $(this).next().children("output")[0];
+    $(counter).text(140 - textLength);
+
+    if (textLength >= 140) {
+      counter.css("color:#FF0000");
+    }
+  });
+});
