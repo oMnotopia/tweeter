287d52d6ddfc99270ffa985429d7fae8760f7de3
Clayton <persinger.clayton@gmail.com>
2023-05-23 19:29:48 -0700

    Update to push new tweets on to the screen without page refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,55 +4,54 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (const tweet of tweets) {
-    // calls createTweetElement for each tweet
-    let tweetValue = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('#old-tweets').append(tweetValue);
-  }
-};
-
-const createTweetElement = (tweetData) => {
-  const $tweet = $("#old-tweets").append(`
-    <article>
-      <header>
-        <div class="icon-name">
-          <img src="${tweetData.user.avatars}" alt="avatar">
-          <p>${tweetData.user.name}</p>
-        </div>
-        <div class="username">${tweetData.user.handle}</div>
-      </header>
-      <p class="old-tweet-text">${tweetData.content.text}</p>
-      <footer>
-        <p>${timeago.format(tweetData.created_at)}</p>
-        <div class="social-tags">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-    `);
-  return $tweet;
-};
-
-const loadTweets = () => {
-  $.ajax({
-    url: "/tweets",
-    success: function(data) {
-      data = data.sort((a,b) => b.created_at - a.created_at);
-      renderTweets(data);
-    },
-    dataType: "json"
-  });
-};
-
-loadTweets();
-
 $(document).ready(() => {
+  const renderTweets = function(tweets) {
+    const oldTweetContainer = $("#old-tweets");
+    oldTweetContainer.empty();
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const tweetValue = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#old-tweets').append(tweetValue);
+    }
+  };
+
+  const createTweetElement = (tweetData) => {
+    const $tweet = `
+      <article id="tweet-test">
+        <header>
+          <div class="icon-name">
+            <img src="${tweetData.user.avatars}" alt="avatar">
+            <p>${tweetData.user.name}</p>
+          </div>
+          <div class="username">${tweetData.user.handle}</div>
+        </header>
+        <p class="old-tweet-text">${tweetData.content.text}</p>
+        <footer>
+          <p>${timeago.format(tweetData.created_at)}</p>
+          <div class="social-tags">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+      `;
+    return $tweet;
+  };
+
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      success: function(data) {
+        data = data.sort((a,b) => b.created_at - a.created_at);
+        renderTweets(data);
+      },
+      dataType: "json"
+    });
+  };
+  
   $("form").on("submit", function(e) {
     e.preventDefault();
     const data = $(this).serialize().slice(5);
@@ -69,9 +68,14 @@ $(document).ready(() => {
       type: "POST",
       url: "/tweets",
       data: {text: data},
+      success: function() {
+        loadTweets();
+      }
     });
   });
+  loadTweets();
 });
+
 
 
 
