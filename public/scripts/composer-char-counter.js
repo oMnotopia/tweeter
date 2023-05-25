194d4e65fe2b941360b0e7074c560c9b867b16e2
Clayton <persinger.clayton@gmail.com>
2023-05-24 21:59:33 -0700

    Create scroll button that scrolls to top of screen

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 $(document).ready(() => {
+  //Counts number of characters entered in textarea.
+  //If number is greater than 140 turn counter red, otherwise default colour.
   $("#tweet-text").on("input", function(e) {
     let textLength = e.target.value.length;
     const counter = $(this).next().children("output")[0];
@@ -10,5 +12,26 @@ $(document).ready(() => {
     } else {
       $(counter).css("color", "");
     }
+  });
+
+  //When user scrolls away from top, a scroll button appears and new tweet toggle dissapears.
+  //When user scrolls to top, scroll button dissapears and new tweet toggle reappears.
+  $(document).on("scroll", function() {
+    if ($(document).scrollTop() !== 0) {
+      $(".toggle-scroll").toggleClass("hidden", false);
+      $(".toggles").toggleClass("hidden", true);
+    }
+
+    if ($(document).scrollTop() === 0) {
+      $(".toggle-scroll").toggleClass("hidden", true);
+      $(".toggles").toggleClass("hidden", false);
+    }
+
+  });
+
+  //If user clicks scroll button returns user to top of screen and focuses text area.
+  $(".toggle-scroll").on("click", function() {
+    $(document).scrollTop(0);
+    $("#tweet-text").focus();
   });
 });
